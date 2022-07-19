6b146f747cad72fe64ea9df0cd9b8ee9eab274a3
FannyKaroliine <stephanie.karoline@live.com>
2022-07-19 18:34:58 -0300

    exercicios de tipos de dados em java

---
+++ src/ExerciciosTiposdeDados/ExerciciosTiposdeDados.java
@@ -0,0 +1,50 @@
+public class ExerciciosTiposdeDados {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = -32560;
+        //short s2 = 40000; valor superior ao suportado pelo tipo
+
+        int i1 = -500;
+        int i2 = 500;
+
+        long l1 = -5000000000000000000L;
+        long l2 = 5000000000000000000L;
+
+        float f1 = 5.5F;
+        float f2 = 10.35F;
+
+        double d1 = 90.56;
+        double d2 = 100.47D;
+
+        char c1 = 'S';
+        char c2 = '\u0357';
+
+        String st1 = "Stephanie";
+        String st2 = "Karoline";
+
+        boolean boo1 = true;
+        boolean boo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(boo1);
+        System.out.println(boo2);
+    }
+}
